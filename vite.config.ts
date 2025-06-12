ed4ffdf054c06df5113c1035fe81d439e83f987e
Franco Sanchez <francosanchez@Francos-MacBook-Pro.local>
2025-06-11 22:56:31 -0300

    @ URLS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+      "@views": fileURLToPath(new URL("./src/views", import.meta.url)),
+      "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+    },
+  },
+});
